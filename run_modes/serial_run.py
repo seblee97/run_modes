8d176a5d5c6cfd735497d61456e25ed6dbb8ccd9
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-11-03 12:33:01 +0000

    type hinting and run methods arg doc

--- run_modes/serial_run.py
+++ run_modes/serial_run.py
@@ -1,12 +1,13 @@
 import os
-from typing import List
+from typing import List, Type
 
-from run_modes import constants, single_run, utils
+from config_manager import base_configuration
+from run_modes import base_runner, constants, single_run, utils
 
 
 def serial_run(
-    runner_class,
-    config_class,
+    runner_class: Type[base_runner.BaseRunner],
+    config_class: Type[base_configuration.BaseConfiguration],
     run_methods: List[str],
     config_path: str,
     checkpoint_paths: List[str],
@@ -17,6 +18,7 @@ def serial_run(
     Args:
         runner_class: runner class to be instantiated.
         config_class: configuration class to be instantiated.
+        run_methods: list of methods to be called on runner class.
         config_path: path to yaml configuration file for experiment.
         checkpoint_paths: list of paths to directories to output results.
         stochastic_packages: list of packages (by name) for which seeds are to be set.
