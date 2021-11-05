c38fb023e463f1a0b24f496d4d859ab2d7530f63
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-11-05 12:23:20 +0000

    add full path for config and results path

--- example/main.py
+++ example/main.py
@@ -1,6 +1,10 @@
+import os
+
 import example_config
 import example_runner
 from run_modes import single_run, utils
+
+MAIN_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == "__main__":
 
@@ -8,13 +12,15 @@ if __name__ == "__main__":
     config = example_config.ExampleConfig
 
     single_checkpoint_path = utils.setup_experiment(
-        mode="single", results_folder="example_results", config_path="config.yaml"
+        mode="single",
+        results_folder=os.path.join(MAIN_FILE_PATH, "example_results"),
+        config_path=os.path.join(MAIN_FILE_PATH, "config.yaml"),
     )
 
     single_run.single_run(
         runner_class=runner,
         config_class=config,
         run_methods=["train", "plot"],
-        config_path="config.yaml",
+        config_path=os.path.join(MAIN_FILE_PATH, "config.yaml"),
         checkpoint_path=single_checkpoint_path,
     )
